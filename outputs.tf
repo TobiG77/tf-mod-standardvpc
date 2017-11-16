ed45dbf86dee9cb7453d59aa1eab6ff25c3ad16a
Tobias Gerschner <tobias@deepthought>
2017-11-16 13:09:47 +1300

    add basic output parameters

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
-# output "current_region" {
-#   value = "${var.region}"
-# }
+output "vpc_id" {
+  value = "${aws_vpc.legacy.id}"
+}
+
+output "private_subnets" {
+  value = ["${aws_subnet.private_a.id}", "${aws_subnet.private_b.id}", "${aws_subnet.private_c.id}"]
+}
+
+output "public_subnets" {
+  value = ["${aws_subnet.public_a.id}", "${aws_subnet.public_b.id}", "${aws_subnet.public_c.id}"]
+}
+
+output "availability_zones" {
+  value = ["${var.region}a", "${var.region}b", "${var.region}c"]
+}
+
+output "vpc_cidr" {
+  value = "${var.vpc_legacy_cidr}"
+}
