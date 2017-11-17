cf66f155e6b06c4a2ad88ca409a7c935a2593156
Tobias Gerschner <tobias@deepthought>
2017-11-17 20:22:45 +1300

    add rds_subnets to outputs

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,10 @@ output "public_subnets" {
   value = ["${aws_subnet.public_a.id}", "${aws_subnet.public_b.id}", "${aws_subnet.public_c.id}"]
 }
 
+output "rds_subnets" {
+  value = ["${aws_subnet.rds_a.id}", "${aws_subnet.rds_b.id}", "${aws_subnet.rds_c.id}"]
+}
+
 output "availability_zones" {
   value = ["${var.region}a", "${var.region}b", "${var.region}c"]
 }
